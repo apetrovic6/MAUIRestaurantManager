b689297b23ce93ded8c99acd897605b7b1ac63ee
apetrovic6 <petrovicante6@gmail.com>
2023-01-09 15:02:14 +0100

    Initial implementation of RestaurantService

---
+++ RestaurantManagerClient/Services/RestaurantService.cs
@@ -0,0 +1,26 @@
+﻿using DevExpress.Xpo;
+using RestaurantManagerClient.Models;
+using RestaurantManagerClient.Repositories;
+
+namespace RestaurantManagerClient.Services;
+
+public class RestaurantService
+{
+    private Repository<Restaurant> RestaurantRepository { get; set; }
+    
+    public RestaurantService(Repository<Restaurant> restaurantRepository)
+    {
+        RestaurantRepository = restaurantRepository;
+    }
+
+    public List<Restaurant> GetAllRestaurants() 
+    {
+         return RestaurantRepository.GetAll(new UnitOfWork()).Result.ToList();
+    }
+
+    public void AddNewRestaurant(Restaurant restaurant)
+    {
+        RestaurantRepository.Save(restaurant);
+    }
+    
+}
