6b0fd9854fd098189f773b0221594e5eaec1bc47
apetrovic6 <petrovicante6@gmail.com>
2023-01-09 15:04:17 +0100

    Initialize MainPage ViewModel

---
+++ RestaurantManagerClient/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,34 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using RestaurantManagerClient.Models;
+using RestaurantManagerClient.Services;
+
+namespace RestaurantManagerClient.ViewModels;
+
+public partial class MainPageViewModel : ObservableObject
+{
+    private RestaurantService RestaurantService { get; set; } 
+
+    [ObservableProperty]
+    private List<Restaurant> _restaurants;
+
+    [ObservableProperty] private Restaurant _newRestaurant = new();
+    
+    public MainPageViewModel(RestaurantService restaurantService)
+    {
+        RestaurantService = restaurantService;
+        GetRestaurants();
+    }
+
+    private  void GetRestaurants()
+    {
+        Restaurants = RestaurantService.GetAllRestaurants();
+    }
+    
+    [RelayCommand]
+    private void AddNewRestaurant()
+    {
+        RestaurantService.AddNewRestaurant(NewRestaurant);
+        GetRestaurants();
+    }
+}
