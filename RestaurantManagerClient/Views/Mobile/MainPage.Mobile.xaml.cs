bc904b0e28b2e622542d5c7ccc26dd002775b008
apetrovic6 <petrovicante6@gmail.com>
2023-01-09 15:06:19 +0100

    Init Mobile Main Page

---
+++ RestaurantManagerClient/Views/Mobile/MainPage.Mobile.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantManagerClient.Views.Mobile;
+
+public partial class MainPageMobile : ContentPage
+{
+    public MainPageMobile()
+    {
+        InitializeComponent();
+    }
+}
