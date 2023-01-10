5cbf52a13f22148842b8444d161a110c6a4e81f3
apetrovic6 <petrovicante6@gmail.com>
2023-01-10 12:26:38 +0100

    Initial MenuService implementation

---
+++ RestaurantManagerClient/Services/MenuService.cs
@@ -0,0 +1,62 @@
+﻿using DevExpress.Xpo;
+using RestaurantManagerClient.Models;
+using RestaurantManagerClient.Repositories;
+
+namespace RestaurantManagerClient.Services;
+
+public class MenuService
+{
+    private Repository<Menu> MenuRepository { get; set; }
+    
+    public MenuService(Repository<Menu> menuRepository)
+    {
+        MenuRepository = menuRepository;
+    }
+
+    public List<Menu> GetAll()
+    {
+        // using (var uow = new UnitOfWork())
+        // {
+        //     return uow.Query<Menu>().Where(x => x.Restaurant.Id == "ab8dbb32-29a2-48fc-8035-43b22b8356d9")
+        //         .ToList();
+        // }
+        return MenuRepository.GetAll(new UnitOfWork()).Result.ToList();
+    }
+
+    public Menu GetMenuById(string id)
+    {
+        return MenuRepository.GetById(id).Result;
+    }
+
+    public void AddNewMenu(string newName, string resId)
+    {
+        using (var uow = new UnitOfWork())
+        {
+            var restaurant = uow.GetObjectByKey<Restaurant>(resId);
+
+            Menu menu = new();
+            
+            menu.Id = Guid.NewGuid().ToString();
+            menu.Name = newName;
+            menu.Restaurant = restaurant;
+            MenuRepository.Save(menu, uow);
+        }
+        
+    }
+
+    public void UpdateMenu(Menu menu)
+    {
+        using var uow = new UnitOfWork();
+        var itemToUpdate = uow.GetObjectByKey<Menu>(menu.Id);
+        
+        if (itemToUpdate == null) return;
+
+        itemToUpdate.Name = menu.Name;
+        MenuRepository.Update(itemToUpdate, uow);
+    }
+
+    public void RemoveMenu(string id)
+    {
+        MenuRepository.Delete(id);
+    } 
+}
