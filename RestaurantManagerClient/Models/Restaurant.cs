e1dc019a3b5dc509007b1e4df729abd8c3de9086
apetrovic6 <petrovicante6@gmail.com>
2023-01-10 12:21:30 +0100

    Add One to many relationship with Menus

--- RestaurantManagerClient/Models/Restaurant.cs
+++ RestaurantManagerClient/Models/Restaurant.cs
@@ -7,4 +7,7 @@ public class Restaurant : BaseModel
 {
     [Size(50)]
     public string Name { get; set; }
+    
+    [Association("Menus-Restaurant")] 
+    public XPCollection<Menu> Menus { get; set; }
 }
