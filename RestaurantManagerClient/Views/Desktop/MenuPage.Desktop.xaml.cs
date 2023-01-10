33904a4c66ce654ab446939fb05a44ec480ab45c
apetrovic6 <petrovicante6@gmail.com>
2023-01-10 12:27:27 +0100

    Initial MenuPage Desktop implementation

---
+++ RestaurantManagerClient/Views/Desktop/MenuPage.Desktop.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RestaurantManagerClient.ViewModels;
+
+namespace RestaurantManagerClient.Views.Desktop;
+
+public partial class MenuPageDesktop : ContentPage
+{
+    public MenuPageDesktop(MenuPageViewModel vm)
+    {
+        InitializeComponent();
+        BindingContext = vm;
+    }
+}
