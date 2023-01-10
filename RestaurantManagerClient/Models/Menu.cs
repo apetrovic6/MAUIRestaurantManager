466a0c35cf9f3a76feb34c9bf12e21d4ac372961
apetrovic6 <petrovicante6@gmail.com>
2023-01-10 12:20:54 +0100

    Init base menu class

---
+++ RestaurantManagerClient/Models/Menu.cs
@@ -0,0 +1,13 @@
+﻿using DevExpress.Xpo;
+
+namespace RestaurantManagerClient.Models;
+
+[Persistent]
+public class Menu : BaseModel
+{
+    [Size(50)]
+    public string Name { get; set; }
+
+    [Association("Menus-Restaurant")] 
+    public Restaurant Restaurant { get; set; }
+}
