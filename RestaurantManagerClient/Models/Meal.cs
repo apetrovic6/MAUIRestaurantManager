7fbec4e925d7e83a682821c6f6f21d985e69fb28
apetrovic6 <petrovicante6@gmail.com>
2023-01-11 10:35:51 +0100

    Add Meal model

---
+++ RestaurantManagerClient/Models/Meal.cs
@@ -0,0 +1,22 @@
+﻿using DevExpress.Xpo;
+
+namespace RestaurantManagerClient.Models;
+
+public class Meal : BaseModel
+{
+    public  Meal(Session session) : base(session) {}
+
+    private string _Name;
+
+    public string Name
+    {
+        get => _Name;
+        set => SetPropertyValue(nameof(Name), ref _Name, value);
+    }
+
+    [Association("Meal-Menu")]
+    public XPCollection<Menu> Menus
+    {
+        get => GetCollection<Menu>(nameof(Menus));
+    }
+}
