0c1f6fc39ffb6a8b885f524f4eb127dbb41c93dc
apetrovic6 <petrovicante6@gmail.com>
2023-01-11 10:36:55 +0100

    Refactor the interface

--- RestaurantManagerClient/Repositories/IGenericRepository.cs
+++ RestaurantManagerClient/Repositories/IGenericRepository.cs
@@ -5,21 +5,10 @@ namespace RestaurantManagerClient.Repositories;
 
     public interface IGenericRepository<T> where T : BaseModel
     {
-        Task<IEnumerable<T>> GetAll(UnitOfWork uow);
-        Task<T> GetById(string id);
+        Task<IEnumerable<T>> GetAll();
+        Task<T> GetById(int id);
         void Update(T obj, UnitOfWork uow);
-        void Delete(string id);
+        void Delete(int id);
         
-        /// <summary>
-        /// Use when saving entities and possible relationship
-        /// </summary>
-        /// <param name="obj">Item to save</param>
-        /// <param name="uow">Unit of Work</param>
         void Save(T obj, UnitOfWork uow);
-
-        /// <summary>
-        /// Use when only saving an entity
-        /// </summary>
-        /// <param name="obj">Item to save</param>
-        void Save(T obj);
     }
