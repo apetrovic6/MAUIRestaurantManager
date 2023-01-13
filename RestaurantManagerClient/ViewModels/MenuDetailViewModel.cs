229d383bc0287bc8b60a8d7e19f4c520be0647e1
apetrovic6 <petrovicante6@gmail.com>
2023-01-13 08:31:56 +0100

    Init MenuDetailPage and ViewModel

---
+++ RestaurantManagerClient/ViewModels/MenuDetailViewModel.cs
@@ -0,0 +1,36 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using RestaurantManagerClient.Models;
+using RestaurantManagerClient.Services;
+
+namespace RestaurantManagerClient.ViewModels;
+
+[QueryProperty(nameof(MenuId), "menuId")]
+public partial class MenuDetailViewModel : ObservableObject
+{
+    private int _menuId;
+    private MenuService MenuService {get; set; }
+
+    [ObservableProperty] 
+    private Menu _fetchedMenu;
+    public int MenuId
+    {
+        get => _menuId;
+        set
+        {
+            if (value == _menuId) return;
+            _menuId = value;
+            OnPropertyChanged();
+            GetMenu();
+        }
+    }
+
+    public MenuDetailViewModel(MenuService menuService)
+    {
+        MenuService = menuService;
+    }
+
+    private void GetMenu()
+    {
+        FetchedMenu = MenuService.GetMenuById(MenuId);
+    }
+}
---
+++ RestaurantManagerClient/Views/Desktop/MenuPages/MenuDetailPage.Desktop.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RestaurantManagerClient.ViewModels;
+
+namespace RestaurantManagerClient.Views.Desktop.MenuPages;
+
+public partial class MenuDetailPageDesktop : ContentPage
+{
+    public MenuDetailPageDesktop(MenuDetailViewModel vm)
+    {
+        InitializeComponent();
+        BindingContext = vm;
+    }
+}
