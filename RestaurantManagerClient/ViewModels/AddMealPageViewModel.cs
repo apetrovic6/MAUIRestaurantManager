f9b091e83f2a5877d456c9b4ca2a0a1876679ad7
apetrovic6 <petrovicante6@gmail.com>
2023-01-15 16:27:35 +0100

    Initial implementation of AddMealPage ViewModel

---
+++ RestaurantManagerClient/ViewModels/AddMealPageViewModel.cs
@@ -0,0 +1,31 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using RestaurantManagerClient.Models;
+using RestaurantManagerClient.Services;
+
+namespace RestaurantManagerClient.ViewModels;
+
+public partial class AddMealPageViewModel : ObservableObject
+{
+    private MealService MealService { get; set; }
+    private RestaurantService RestaurantService { get; set; }
+    private MenuService MenuService { get; set; }
+
+    [ObservableProperty] private Meal _newMeal = new();
+
+    [ObservableProperty] private List<Menu> _menus;
+
+    [ObservableProperty] private Menu _selectedMenu = new();
+
+    public AddMealPageViewModel(MealService mealService, RestaurantService restaurantService, MenuService menuService)
+    {
+        MealService = mealService;
+    }
+
+    [RelayCommand]
+    private async void AddNewMeal()
+    {
+        MealService.AddNewMeal(NewMeal);
+        await Shell.Current.GoToAsync("..");
+    }
+}
