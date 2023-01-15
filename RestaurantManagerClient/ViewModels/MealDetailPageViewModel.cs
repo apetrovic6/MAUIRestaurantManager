542f2e8c932b585d07aa76c98ababe9a2804de55
apetrovic6 <petrovicante6@gmail.com>
2023-01-15 16:36:35 +0100

    Init MealDetailPage resources

---
+++ RestaurantManagerClient/ViewModels/MealDetailPageViewModel.cs
@@ -0,0 +1,36 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using RestaurantManagerClient.Models;
+using RestaurantManagerClient.Services;
+
+namespace RestaurantManagerClient.ViewModels;
+
+[QueryProperty(nameof(MealId), "menuId")]
+public partial class MealDetailPageViewModel : ObservableObject
+{
+    private int _mealId;
+    private MealService MealService { get; set; }
+
+    public int MealId
+    {
+        get => _mealId;
+        set
+        {
+            if (value == _mealId) return;
+            _mealId = value;
+            OnPropertyChanged();
+            GetMeal();
+        }
+    }
+
+    [ObservableProperty] private Meal _fetchedMeal;
+    
+    public MealDetailPageViewModel(MealService mealService)
+    {
+        MealService = mealService;
+    }
+
+    private void GetMeal()
+    {
+        FetchedMeal = MealService.GetMealById(MealId);
+    }
+}
