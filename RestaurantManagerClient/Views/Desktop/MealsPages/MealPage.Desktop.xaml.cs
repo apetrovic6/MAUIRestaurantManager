ea84bb1783d97e128b708a3a81322def0d56435c
apetrovic6 <petrovicante6@gmail.com>
2023-01-15 16:29:18 +0100

    Initial implementation of MealPage

--- RestaurantManagerClient/Views/Desktop/MealsPages/MealPage.Desktop.xaml.cs
+++ RestaurantManagerClient/Views/Desktop/MealsPages/MealPage.Desktop.xaml.cs
@@ -3,13 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using RestaurantManagerClient.ViewModels;
 
 namespace RestaurantManagerClient.Views.Desktop.MealsPages;
 
 public partial class MealPageDesktop : ContentPage
 {
-    public MealPageDesktop()
+    public MealPageDesktop(MealPageViewModel vm)
     {
         InitializeComponent();
+        BindingContext = vm;
+    }
+
+    protected override void OnAppearing()
+    {
+        base.OnAppearing();
+    
+        var vm = (MealPageViewModel)BindingContext;
+    
+        vm.GetMeals();
     }
 }
