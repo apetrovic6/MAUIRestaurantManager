1384175d5a3bd44c8d63e8f8c34712f243469f90
apetrovic6 <petrovicante6@gmail.com>
2023-01-15 16:32:05 +0100

    Display current meals, remove meals from the menu, show all meals, add meals to the menu

--- RestaurantManagerClient/ViewModels/MenuDetailViewModel.cs
+++ RestaurantManagerClient/ViewModels/MenuDetailViewModel.cs
@@ -1,4 +1,7 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+﻿using System.Collections.ObjectModel;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using DevExpress.Xpo;
 using RestaurantManagerClient.Models;
 using RestaurantManagerClient.Services;
 
@@ -9,9 +12,16 @@ public partial class MenuDetailViewModel : ObservableObject
 {
     private int _menuId;
     private MenuService MenuService {get; set; }
-
+    private MealService MealService {get; set; }
+    
     [ObservableProperty] 
     private Menu _fetchedMenu;
+
+    [ObservableProperty] private List<Meal> _meals;
+
+    [ObservableProperty]
+    private List<object> _selectedMeals = new();
+    
     public int MenuId
     {
         get => _menuId;
@@ -24,13 +34,43 @@ public partial class MenuDetailViewModel : ObservableObject
         }
     }
 
-    public MenuDetailViewModel(MenuService menuService)
+    public MenuDetailViewModel(MenuService menuService, MealService mealService)
     {
         MenuService = menuService;
+        MealService = mealService;
+        
+        GetMeals();
     }
 
     private void GetMenu()
     {
         FetchedMenu = MenuService.GetMenuById(MenuId);
+    }
+
+    private void GetMeals()
+    {
+        Meals = MealService.GetAllMeals();
+    }
+    
+    [RelayCommand]
+    private async void RemoveMenu()
+    {
+        MenuService.RemoveMenu(MenuId);
+        await Shell.Current.GoToAsync("..");
+    }
+
+    [RelayCommand]
+    private void AddMealsToMenu()
+    {
+        var mealList = SelectedMeals.Cast<Meal>().ToList();
+        MenuService.UpdateMenu(FetchedMenu, mealList);
+        GetMenu();
+    }
+
+    [RelayCommand]
+    private void RemoveMealFromMenu(Meal meal)
+    {
+        MenuService.RemoveMeal(MenuId, meal);
+        GetMenu();
     }
 }
