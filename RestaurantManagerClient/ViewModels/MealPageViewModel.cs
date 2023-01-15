10a63f0416d77d6073482c246c55cbed5dda7987
apetrovic6 <petrovicante6@gmail.com>
2023-01-15 16:35:35 +0100

    Initial implementation of MealPage ViewModel

---
+++ RestaurantManagerClient/ViewModels/MealPageViewModel.cs
@@ -0,0 +1,35 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using RestaurantManagerClient.Models;
+using RestaurantManagerClient.Services;
+
+namespace RestaurantManagerClient.ViewModels;
+
+public partial class MealPageViewModel : ObservableObject
+{
+    private MealService MealService { get; set; }
+    [ObservableProperty] private List<Meal> _meals;
+
+    public MealPageViewModel(MealService mealService)
+    {
+        MealService = mealService;
+    }
+    
+    
+    public void GetMeals()
+    {
+        Meals = MealService.GetAllMeals();
+    }
+
+    [RelayCommand]
+    private async void GoToAddMeal()
+    {
+        await Shell.Current.GoToAsync("AddMealPage");
+    }
+
+    [RelayCommand]
+    private async void GoToMeal()
+    {
+        
+    }
+}
